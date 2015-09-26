528764ad3edb19ccfe48404c0cd50c82bec4e8a6
mariekamili <mariethakamili.com>
2015-09-26 10:50:26 +0300

    first project

---
+++ src/first_work/First_Work.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first_work;
+
+/**
+ *
+ * @author sandra kamili
+ */
+public class First_Work {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("hello weekend");
+    }
+}
